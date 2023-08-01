b76d438078a96f7c74a65bca63bbe07ade6d3456
Saurabh0703 <66934981+Saurabh0703@users.noreply.github.com>
2023-08-01 10:41:47 +0530

    Update pritunl.tf

--- pritunl.tf
+++ pritunl.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "ec2" {
   ami                     = data.aws_ami.ubuntu.id
   instance_type           = var.instance_type
   subnet_id               = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id == "" ? aws_security_group.default[0].id : var.security_group_id]
+  #vpc_security_group_ids = [var.security_group_id == "" ? aws_security_group.default[0].id : var.security_group_id]
   key_name                = var.key_name
   ebs_optimized           = var.ebs_optimized
   disable_api_termination = var.disable_api_termination
